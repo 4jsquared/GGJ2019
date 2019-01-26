6ffceb6cd3a6bb18e4e92e9790da00bf3c56b13a
Jason Wyatt <jasonwyatt795@gmail.com>
2019-01-26 20:18:29 +0000

    Add some audio, a pile of money, the clock etc...

---
+++ Assets/Scripts/MoneyPile.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyPile : MonoBehaviour
+{
+	[SerializeField] private SpriteRenderer[] moneySprites;
+	[SerializeField] private float moneyMax;
+
+	[SerializeField] private Player player;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		int numSprites = (int)(moneySprites.Length * player.money / moneyMax);
+
+		foreach (SpriteRenderer renderer in moneySprites)
+		{
+			if (numSprites > 0)
+				renderer.enabled = true;
+			else
+				renderer.enabled = false;
+
+			numSprites--;
+		}
+	}
+}
