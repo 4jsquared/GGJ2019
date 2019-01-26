c47a996782b9d8ba46b80905cb43e635a2adcb23
jkblilly <jlilly@live.com>
2019-01-26 18:17:45 +0000

    Add files via upload

---
+++ Assets/Scripts/ClockSpinner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClockSpinner : MonoBehaviour {
+
+    public float spinSpeed;
+    private bool timeToMove = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (timeToMove == true)
+        {
+            transform.Rotate(0, 0, spinSpeed * -Time.deltaTime);
+        }
+    }
+}
